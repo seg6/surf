25754a32d7c1b271af858b91ce949b4b9cb0cbb3
seg6 <hi@seg6.space>
2026-07-28 00:21:31 +0200

    runtime: validate Windows browser file versions

---
+++ backend/internal/browserbin/version_unix.go
@@ -0,0 +1,26 @@
+//go:build !windows
+
+package browserbin
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+func browserMajor(path string) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	output, err := exec.CommandContext(ctx, path, "--version").CombinedOutput()
+	if err != nil {
+		return 0, err
+	}
+	match := regexp.MustCompile(`\b(\d+)\.`).FindSubmatch(output)
+	if len(match) != 2 {
+		return 0, fmt.Errorf("browser version missing from %q", output)
+	}
+	return strconv.Atoi(string(match[1]))
+}
